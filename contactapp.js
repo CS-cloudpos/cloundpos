0fb6dc33bd99d9f76492955154452eb52c5edb27
CS-cloudpos <131598818+CS-cloudpos@users.noreply.github.com>
2023-04-25 02:34:46 +0700

    Add files via upload

--- contactapp.js
+++ contactapp.js
@@ -1,25 +1,28 @@
-const fname =document.getElementById('fname')
-const email =document.getElementById('email')
-const massage =document.getElementById('massage')
-const submit =document.getElementById('sumbit')
 
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
+
+function sendEmail(){
+
+    var fname = document.getElementById('fname').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('massage').value;
+
+    console.log("Clicked")
+
     let ebody = `
-    <h1>Full Name: </h1>${fname.value}
-    <br>
-    <h1>Email: </h1>${email.value}
-    <br>
-    <h1>Massage: </h1>${massage.value}
+        <b>Full Name: </b>${fname};
+        <br>    
+        <b>Email: </b>${email};
+        <br>
+        <b>Message: </b>${message};
     `;
 
     Email.send({
         SecureToken : "c1cf74d0-7e5b-4c0b-bd81-ddf51138c1f7",
         To : 'cs.foodcloudpos@gmail.com', 
-        From : email.value,
-        Subject : "" + email.value,
+        From : 'cs.foodcloudpos@gmail.com',
+        Subject : "Cloud POS problem report from " + email,
         Body : ebody
-    }).then(
-      message => alert(message)
-    );
-});
+    }).then(function() {
+        alert('Email sent successfully!');
+    });
+}
